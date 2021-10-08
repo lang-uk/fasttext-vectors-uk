500eb75a4f33bdeb046aa6feb3277637d7e963b9
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2021-10-08 16:25:10 +0300

    Adding argparse/logging/pathlib, logging of different verbosity, filenames with params, etc

--- callbacks.py
+++ callbacks.py
@@ -1,38 +1,47 @@
-from gensim.models.callbacks import CallbackAny2Vec
-import gensim
+import os.path
 import time
+import logging
+
+import gensim
+from gensim.models.callbacks import CallbackAny2Vec
+
+logger = logging.getLogger("fasttext")
 
 
 class EpochSaver(CallbackAny2Vec):
-    '''
+    """
     Callback to save model after each epoch.
-    '''
+    """
 
-    def __init__(self, path_prefix, save_nth_epoch):
-        self.path_prefix = path_prefix
-        self.save_nth_epoch = save_nth_epoch
+    def __init__(self, settings):
+        self.settings = settings
         self.epoch = 0
 
     def on_epoch_end(self, model):
-        if self.epoch % self.save_nth_epoch == 0:
-            gensim.models.fasttext.save_facebook_model(model, f"{self.path_prefix}_epoch{self.epoch}.bin")
+        if (self.epoch + 1) % self.settings.save_nth_epoch == 0:
+            gensim.models.fasttext.save_facebook_model(
+                model,
+                str(self.settings.path_save_model
+                / f"{os.path.basename(self.settings.corpus_path)}.d{self.settings.vectors_size}.subword{self.settings.min_ngram}-{self.settings.max_ngram}.{self.settings.algo}.epoch{self.epoch}.bin"),
+            )
+
         self.epoch += 1
 
 
 class EpochLogger(CallbackAny2Vec):
-    '''
+    """
     Callback to log information about training
-    '''
-    
+    """
+
     def __init__(self):
         self.epoch = 0
         self.epoch_time_start = 0
 
     def on_epoch_begin(self, model):
         self.epoch_time_start = time.time()
-        print(f"Epoch #{self.epoch} start")
+        logger.info(f"Epoch #{self.epoch} start")
 
     def on_epoch_end(self, model):
-        print(f"Time for training on {self.epoch} epoch: {time.time() - self.epoch_time_start}")
-        print(f"Epoch #{self.epoch} end")
+        logger.info(f"Time for training on {self.epoch} epoch: {time.time() - self.epoch_time_start}")
+        logger.info(f"Epoch #{self.epoch} end")
         self.epoch += 1
--- train_fasttext.py
+++ train_fasttext.py
@@ -1,33 +1,68 @@
+import sys
+import pathlib
+import argparse
+import logging
+
 from gensim.models.fasttext import FastText
 from callbacks import EpochLogger, EpochSaver
-import sys
 
 
-def train_model(corpus_file, vec_size, algorithm, num_epoch, path_to_save_model, save_nth_epoch, min_n, max_n):
+logger = logging.getLogger("fasttext")
+
+
+def train_model(settings):
+    assert settings.min_ngram < settings.max_ngram, "Min length of ngram cannot be greater or equal to the max length"
     epoch_logger = EpochLogger()
-    epoch_saver = EpochSaver(path_to_save_model, save_nth_epoch)
-    sg = 1 if algorithm == "skipgram" else 0
-    model = FastText(vector_size=vec_size)
+    epoch_saver = EpochSaver(settings)
+    sg = 1 if settings.algo == "skipgram" else 0
+    model = FastText(vector_size=settings.vectors_size)
 
     # build the vocabulary
-    print("\nBuilding vocabulary...\n")
-    model.build_vocab(corpus_file=corpus_file)
+    logger.info("Building vocabulary...")
+    model.build_vocab(corpus_file=str(settings.corpus_path))
     total_words = model.corpus_total_words
 
-    print("\nStarted training...\n")
+    logger.info("Started training...")
     # train the model
 
-    model.train(corpus_file=corpus_file, sg = sg, total_words=total_words, epochs=num_epoch, min_n=min_n, max_n=max_n, callbacks=[epoch_logger, epoch_saver])
+    model.train(
+        corpus_file=str(settings.corpus_path),
+        sg=sg,
+        total_words=total_words,
+        epochs=settings.n_epoch,
+        min_n=settings.min_ngram,
+        max_n=settings.max_ngram,
+        callbacks=[epoch_logger, epoch_saver],
+    )
 
 
 if __name__ == "__main__":
-    # arguments reading
-    corpus_path = sys.argv[1]
-    vectors_size = int(sys.argv[2])
-    algo = sys.argv[3]
-    n_epoch = int(sys.argv[4])
-    path_save_model = sys.argv[5]
-    save_nth_weights = int(sys.argv[6])
-    min_ngram = int(sys.argv[7])
-    max_ngram = int(sys.argv[8])
-    train_model(corpus_path, vectors_size, algo, n_epoch, path_save_model, save_nth_weights, min_ngram, max_ngram)
+    parser = argparse.ArgumentParser(description="Train fasttext vectors using gensim.")
+    parser.add_argument("corpus_path", type=pathlib.Path, help="Path to the corpus text file")
+    parser.add_argument("--vectors_size", type=int, help="Dimensions of the vectors", default=300)
+    parser.add_argument("--n_epoch", type=int, help="Number of epochs", default=15)
+    parser.add_argument("--save_nth_epoch", type=int, help="Save every nth epoch", default=5)
+    parser.add_argument("--min_ngram", type=int, help="Min ngram", default=3)
+    parser.add_argument("--max_ngram", type=int, help="Max ngram", default=6)
+    parser.add_argument("path_save_model", type=pathlib.Path, help="Path to save checkpoints")
+    parser.add_argument("--algo", type=str, help="Skipgrams or CBOW", choices=("skipgram", "cbow"), default="skipgram")
+    parser.add_argument("--verbosity", type=int, default=1, choices=(0, 1, 2, 3))
+    args = parser.parse_args()
+
+    logging.basicConfig(format="%(name)s:%(levelname)s %(asctime)s %(message)s")
+    gensim_logger = logging.getLogger("gensim")
+
+    if args.verbosity == 0:
+        logger.setLevel(level=logging.WARNING)
+        gensim_logger.setLevel(logging.WARNING)
+    elif args.verbosity == 1:
+        logger.setLevel(level=logging.INFO)
+        gensim_logger.setLevel(logging.WARNING)
+    elif args.verbosity == 2:
+        logger.setLevel(level=logging.DEBUG)
+        gensim_logger.setLevel(logging.INFO)
+    elif args.verbosity == 3:
+        logger.setLevel(level=logging.DEBUG)
+        gensim_logger.setLevel(logging.DEBUG)
+
+    train_model(args)
